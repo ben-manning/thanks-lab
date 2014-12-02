9c9e4e9ed51a60260afbf283eb5602cb94c4959b
Ben Manning <bmanning@me.com>
2014-12-02 13:41:19 -0800

    make changes to memory

---
+++ memory.js
@@ -0,0 +1,82 @@
+
+
+
+
+window.onload = function() {
+	//boxes
+	var squareOne      = document.querySelector("#square_1");
+	var squareTwo      = document.querySelector("#square_2");
+	var squareThree    = document.querySelector("#square_3");
+	var squareFour     = document.querySelector("#square_4");
+	var squareFive     = document.querySelector("#square_5");
+	var squareSix      = document.querySelector("#square_6");
+	var squareSeven    = document.querySelector("#square_7");
+	var squareEight    = document.querySelector("#square_8");
+	var squareNine     = document.querySelector("#square_9");
+	var squareTen      = document.querySelector("#square_10");
+	var squareEleven   = document.querySelector("#square_11");
+	var squareTwelve   = document.querySelector("#square_12");
+	var squareThirteen = document.querySelector("#square_13");
+	var squareFourteen = document.querySelector("#square_14");
+	var squareFifteen  = document.querySelector("#square_15");
+	var squareSixteen  = document.querySelector("#square_16");
+
+    //letters
+    var letterA = document.querySelector(".A");
+    var letterB = document.querySelector(".B");
+    var letterC = document.querySelector(".C");
+    var letterD = document.querySelector(".D");
+    var letterE = document.querySelector(".E");
+    var letterF = document.querySelector(".F");
+    var letterG = document.querySelector(".G");
+    var letterH = document.querySelector(".H");
+
+	squareOne.onclick = function() {
+    console.log("click function works.");
+     };
+    squareTwo.onclick = function() {
+    console.log("click function works.");
+     };
+    squareThree.onclick = function() {
+    console.log("click function works.");
+     };
+    squareFour.onclick = function() {
+    console.log("click function works.");
+     };
+    squareFive.onclick = function() {
+    console.log("click function works.");
+     };
+    squareSix.onclick = function() {
+    console.log("click function works.");
+     };
+    squareSeven.onclick = function() {
+    console.log("click function works.");
+     };
+    squareEight.onclick = function() {
+    console.log("click function works.");
+     };
+    squareNine.onclick = function() {
+    console.log("click function works.");
+     };
+    squareTen.onclick = function() {
+    console.log("click function works.");
+     };
+    squareEleven.onclick = function() {
+    console.log("click function works.");
+     };
+    squareTwelve.onclick = function() {
+    console.log("click function works.");
+     };
+    squareThirteen.onclick = function() {
+    console.log("click function works.");
+     };
+    squareFourteen.onclick = function() {
+    console.log("click function works.");
+     };
+    squareFifteen.onclick = function() {
+    console.log("click function works.");
+     };
+    squareSixteen.onclick = function() {
+    console.log("click function works.");
+     };
+ };
